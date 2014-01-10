cb8c89b2ffe575d8c19bf880f30c18eb3b49930a
VicisSilentium <kyler_jensen@hotmail.com>
2014-01-10 09:45:39 -0700

    Upload

---
+++ src/stupid/Dumber.java
@@ -0,0 +1,18 @@
+package stupid;
+
+public class Dumber extends Dumb
+{
+	private String derf;
+	
+	public Dumber()
+	{
+		super(9);
+		derf = "ahsdkfjsajghsjhglkasjfdhg;slkjfghs;kguhjsckbn";
+	}
+
+	public String getDerf()
+	{
+		return derf;
+	}
+	
+}
